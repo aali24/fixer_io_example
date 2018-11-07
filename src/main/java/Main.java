1c43cd2a70e0980572f840fdfd3a4dbf9d15c21d
abdi ali <aali@spartaglobal.com>
2018-11-07 11:50:12 +0000

    created keyReader class for api key properties file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+
+        KeyReader keyReader = new KeyReader();
+        System.out.println(keyReader.getApi_key());
+
+    }
+
 }
